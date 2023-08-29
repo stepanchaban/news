f749f851ad9b07614764c28d16e45d2aa6a996f1
Stepan Chaban <aziibro@yandex.ru>
2023-08-29 16:29:02 +0400

    async api_news

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,7 +1,20 @@
+import { useEffect } from 'react';
+import getNews from '../../api/apiNews';
 import NewsBanner from '../../components/NewsBanner/NewsBanner';
 import styles from './Main.module.css';
 
 const Main = () => {
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const news = await getNews();
+        console.log(news);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchNews();
+  }, []);
   return (
     <main className={styles.header}>
       <NewsBanner />
